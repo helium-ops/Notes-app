d53794b52f17d37f9d81c695dfb47ea4550e5f12
helium-ops <mohamedmah1234@outlook.com>
2026-07-05 05:27:46 -0400

    tried to add a new feature but didnt work out

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@ const input = document.querySelector("input");
 const addBtn = document.querySelector(".add-btn");
 const notesArea = document.querySelector(".note-area");
 
+let note = 0;
 addBtn.addEventListener("click", ()=>{
     let note = document.createElement("div");
     note.innerHTML = `
+    <div class="title-view">
+    <input type="text" placeholder="Add a title.....">
     <button class="view"><i class="fa-solid fa-arrows-to-eye"></i></button>
+    </div>
     <p class="text">${input.value}</p>`
+    note = input.value;
     notesArea.appendChild(note);
+    if(document.querySelector(".text").textContent.length > 20){
+        document.querySelector(".text").innerHTML += "...";
+    }
     note.classList.add("note");
-})
+});
+
